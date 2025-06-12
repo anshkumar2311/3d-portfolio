07012d6a410cfb1def88b39fec3c086fddb5e9b3
ANSH KUMAR <anshkumar1399@gmail.com>
2025-06-12 18:34:37 +0530

    struc tailwind ini

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className="text-amber-300">hello</h1>
+      <h1 className="text-4xl text-blue-50 font-black">hello</h1>
     </div>
   )
 }
